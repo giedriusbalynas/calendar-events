13fad10ea78111d333dbcda62f26c2c7faa78289
giedriusbalynas <58655993+giedriusbalynas@users.noreply.github.com>
2020-06-23 19:15:48 +0300

    Connected to google calendar, added router.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import App from './App.vue'
+import Home from './components/Home';
+import vuetify from './plugins/vuetify';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter);
+
+const routes = [
+  {path: '/', component: Home},
+  // {path: '/', component: ""}
+];
+
+const router = new VueRouter({
+  routes,
+  mode: 'history'
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  vuetify,
+  render: h => h(App)
+}).$mount('#app');
